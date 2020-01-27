d66bc8a8d770e8c46db04033be4768937df122be
Rafal Bie <rafalbie@mbp-rafal.home>
2020-01-27 15:15:35 +0100

    commit

--- z-4.js
+++ z-4.js
@@ -1,2 +1,16 @@
 // 4) Umieść 10 tablic wygenerowanych w zadaniu 3, w jednej tablicy.
 
+const randomTab2 = table.fill(0).map(el => Math.floor(Math.random() * 10) + 1);
+
+const newTab = new Array(10);
+for(let i=0; i<10; i++){
+    newTab[i] = new Array (10)
+    for (j=0;j<10;j++){
+    newTab[i] = [randomTab2]
+    }
+    
+    
+} 
+console.log (newTab)
+// console.log(randomTab2)
+
