0d839cda0be0f0e5258873716588815134dbc664
Rafal Bie <rafalbie@mbp-rafal.home>
2020-03-27 17:22:59 +0100

    commit "z-8  to check"

--- z-8.js
+++ z-8.js
@@ -7,39 +7,51 @@
  // oraz _id = objectId() - wykorzystać bibliotekę  https://www.npmjs.com/package/uuid
          
  
-  const  persons = 
+  const  people = 
 
   [
       {"name": "Jan", "surname": "Kowalski"},
       {"name": "John", "surname": "Smith"},
       {"name": "Robin", "surname": "Kenedy"},
    ];
+    const countries = ['PL', 'UK', 'USA'];
    
 
         
  
 
-  const generator = (persons) => { 
-         const indexName = Math.floor(Math.random() * persons.length);
+  const generator = () => { 
+         const indexName = Math.floor(Math.random() * people.length);
          // console.log (`${persons[indexName]}`);
-         return  persons[indexName] 
+         return  people[indexName] 
       };
-     console.log (generator(persons))
+      const generator2 = () =>{
+        const indexContries = Math.floor(Math.random() * countries.length);
+        return countries[indexContries] 
+      };
+      function generators(generator,generator2) {
+        
+        console.log(generator,generator2)
+      };
+      generators(people,countries)
+      
+    //  console.log (generator(people))
+    //  console.log (generator2(countries))
      
-   const generateHuman = (generator) => {
-       const person = generator;
-         return {
-      name: generator.name,
-      }
-   };
+  //  const generateHuman = (generator) => {
+  //      const person = generator;
+  //        return {
+  //     name: generator.name,
+  //     }
+  //  };
    
-       console.log(generateHuman(persons,generator))
+  //      console.log(generateHuman(persons,generator))
 
 
 
 
    
-   //  countries: ['PL', 'UK', 'USA'],
+   //  
    // const emails =  ['gmail.com', 'onet.pl', 'gazeta.pl'];
     // generateEmail = Math.floor(Math.random() * emails.length),
       // generateAge(minAge,maxAge){
